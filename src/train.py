aff42cb7f590565d942adaf930119a1045689547
SCESecure <harry050327@gmail.com>
2026-06-19 02:07:01 +0900

    print 함수로 설명 추가

--- src/train.py
+++ src/train.py
@@ -5,13 +5,19 @@ from sklearn.feature_selection import SelectFromModel
 
 import pandas as pd
 
+print("\n\n")
+print("[Train 단계 (train.py)]")
+print("\n\n")
+
 # RandomForest 사용
 forest = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
 forest.fit(X_train_scaled, y_train)
+print("RandomForest로 하여 X_train_scaled와 y_train fit 완료")
 
 # 특성 선택
 selector = SelectFromModel(forest, prefit=True)
 features_bool = selector.get_support()
+print("선택된 특성의 boolean 값 : " + features_bool)
 
 selected_features = []
 for i in range(0,X_train_scaled.shape[1]) :
@@ -19,5 +25,7 @@ for i in range(0,X_train_scaled.shape[1]) :
         selected_features.insert(i, X_train_scaled.columns[i])
 
 # 선택된 특성 출력 (selected_features)
+print("\n")
 print("RandomForest에 의해 선택된 특성은 다음과 같습니다 : " + selected_features)
 
+# Logistic Regression 학습

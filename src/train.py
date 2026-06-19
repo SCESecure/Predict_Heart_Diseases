86c61430a492c28f1bc82d774f4c0f4f34e1b016
SCESecure <harry050327@gmail.com>
2026-06-19 16:28:01 +0900

    SVC 완료

--- src/train.py
+++ src/train.py
@@ -1,12 +1,19 @@
+# 전처리 했던 변수들 가져옴
 from preprocessing import X_train_scaled, y_train, X_test_scaled, y_test
 
+# 앙상블로 특성 선택에 필요한 라이브러리
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_selection import SelectFromModel
 
+# 모델들
 from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
 
+# 정확도 평가
 from sklearn.metrics import accuracy_score, roc_auc_score
+from sklearn.calibration import CalibratedClassifierCV
 
+# 그 이외의 라이브러리
 import pandas as pd
 
 print("\n\n")
@@ -49,4 +56,37 @@ print("\n")
 print("Logistic Regression 모델 학습 완료")
 print("Acc : " + str(LR_acc) + " ({:.2f}%)".format(LR_acc * 100))
 print("ROC AUC 점수(OVO(one-vs-one)) : " + str(LR_roc_auc) + " ({:.2f}%)".format(LR_roc_auc * 100))
+
+print("\n")
+
+SVM_kernel = ['linear', 'poly', 'rbf', 'sigmoid']
+SVM_acc_list = []
+SVM_roc_acc_list = []
+
+for i in range(0, len(SVM_kernel)) :
+    # SVC의 probality 속성이 scikit-learn 1.11 버전부터 deprecated 됨에 따라
+    # CalibratedClassifierCV() 함수로 이를 대체함
+
+    if (i == 'poly') :
+        SVM_model = CalibratedClassifierCV(SVC(kernel=SVM_kernel[i], C=5, degree=3, random_state=1), ensemble=False)
+    else :
+        SVM_model = CalibratedClassifierCV(SVC(kernel=SVM_kernel[i], C=5, random_state=1), ensemble=False)
+    
+    SVM_model.fit(X_train_scaled, y_train)
+
+    SVM_pred = SVM_model.predict(X_test_scaled)
+    SVM_pred_prob = SVM_model.predict_proba(X_test_scaled)
+    SVM_acc = accuracy_score(y_test, SVM_pred)
+    SVM_roc_acc = roc_auc_score(y_test, SVM_pred_prob, multi_class='ovo')
+
+    SVM_acc_list.insert(i, SVM_acc)
+    SVM_roc_acc_list.insert(i, SVM_roc_acc)
+
+print("SVM(Support Vector Machine) 모델 학습 완료")
+
+for i in range(0, len(SVM_kernel)) :
+    print("[" + SVM_kernel[i] + " 모델] ")
+    print("Acc : " + str(SVM_acc_list[i]) + " ({:.2f}%)".format(SVM_acc_list[i] * 100))
+    print("ROC AUC 점수(OVO(one-vs-one)) : " + str(SVM_roc_acc_list[i]) + " ({:.2f}%)".format(SVM_roc_acc_list[i] * 100))
+    print("")
 

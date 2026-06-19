42a8d51da1244a5193702b3eb57bc3bbdea972a7
SCESecure <harry050327@gmail.com>
2026-06-19 22:28:52 +0900

    SVM 까지 완료

--- src/inference.py
+++ src/inference.py
@@ -1,101 +1,111 @@
-from train import X_test_selected, y_test, \
-    LR_model, SVM_kernel, SVM_fitted_list, RF_model, KNN_model, XGB_model
+# from train import X_test_selected, y_test, \
+#     LR_model, SVM_kernel, SVM_fitted_list, RF_model, KNN_model, XGB_model
 
 from sklearn.metrics import accuracy_score, roc_auc_score
 
-print("\n\n")
-print("[Inference 단계 (inference.py)]")
-print("\n\n")
+def LR_predict(LR_model, X_test_selected, y_test) :
 
-# Logistic Regression 모델 예측 및 점수
-print("Logistic Regression 모델에 대하여 예측합니다.")
-LR_pred = LR_model.predict(X_test_selected)
-LR_pred_prob = LR_model.predict_proba(X_test_selected)
+    # Logistic Regression 모델 예측 및 점수
+    print("Logistic Regression 모델에 대하여 예측합니다.")
+    LR_pred = LR_model.predict(X_test_selected)
+    LR_pred_prob = LR_model.predict_proba(X_test_selected)
 
-LR_acc = accuracy_score(y_test, LR_pred)
-LR_roc_auc_ovo = roc_auc_score(y_test, LR_pred_prob, multi_class='ovo')
-LR_roc_auc_ovr = roc_auc_score(y_test, LR_pred_prob, multi_class='ovr')
+    LR_acc = accuracy_score(y_test, LR_pred)
+    LR_roc_auc_ovo = roc_auc_score(y_test, LR_pred_prob, multi_class='ovo')
+    LR_roc_auc_ovr = roc_auc_score(y_test, LR_pred_prob, multi_class='ovr')
 
-print("정확도 점수는 다음과 같습니다.")
-print("Acc : " + str(LR_acc) + " ({:.2f}%)".format(LR_acc * 100))
-print("ROC AUC 점수(OVO(one-vs-one)) : " + str(LR_roc_auc_ovo) + " ({:.2f}%)".format(LR_roc_auc_ovo * 100))
-print("ROC AUC 점수(OVR(one-vs-rest)) : " + str(LR_roc_auc_ovr) + " ({:.2f}%)".format(LR_roc_auc_ovr * 100))
+    print("정확도 점수는 다음과 같습니다.")
+    print("Acc : " + str(LR_acc) + " ({:.2f}%)".format(LR_acc * 100))
+    print("ROC AUC 점수(OVO(one-vs-one)) : " + str(LR_roc_auc_ovo) + " ({:.2f}%)".format(LR_roc_auc_ovo * 100))
+    print("ROC AUC 점수(OVR(one-vs-rest)) : " + str(LR_roc_auc_ovr) + " ({:.2f}%)".format(LR_roc_auc_ovr * 100))
 
-print("\n")
+    print("\n")
 
-# SVM(Support Vector Machine) 모델 예측 및 점수
-SVM_acc_list = []
-SVM_roc_acc_ovo_list = []
-SVM_roc_acc_ovr_list = []
+    total = [LR_acc, LR_roc_auc_ovo, LR_roc_auc_ovr]
+    return total
 
-print("SVM(Support Vector Machine) 모델에 대하여 예측합니다.")
-for i in range(0, len(SVM_fitted_list)) :
 
-    SVM_pred = SVM_fitted_list[i].predict(X_test_selected)
-    SVM_pred_prob = SVM_fitted_list[i].predict_proba(X_test_selected)
+def SVM_predict(SVM_model, X_test_selected, y_test) :
+
+    # SVM(Support Vector Machine) 모델 예측 및 점수
+    SVM_pred = SVM_model.predict(X_test_selected)
+    SVM_pred_prob = SVM_model.predict_proba(X_test_selected)
 
     SVM_acc = accuracy_score(y_test, SVM_pred)
     SVM_roc_acc_ovo = roc_auc_score(y_test, SVM_pred_prob, multi_class='ovo')
     SVM_roc_acc_ovr = roc_auc_score(y_test, SVM_pred_prob, multi_class='ovr')
+    
+    print("SVM(Support Vector Machine) 모델에 대하여 예측합니다.")
 
-    SVM_acc_list.insert(i, SVM_acc)
-    SVM_roc_acc_ovo_list.insert(i, SVM_roc_acc_ovo)
-    SVM_roc_acc_ovr_list.insert(i, SVM_roc_acc_ovr)
+    print("Acc : " + str(SVM_acc) + " ({:.2f}%)".format(SVM_acc * 100))
+    print("ROC AUC 점수(OVO(one-vs-one)) : " + str(SVM_roc_acc_ovo) + " ({:.2f}%)".format(SVM_roc_acc_ovo * 100))
+    print("ROC AUC 점수(OVR(one-vs-rest)) : " + str(SVM_roc_acc_ovr) + " ({:.2f}%)".format(SVM_roc_acc_ovr * 100))
 
-print("정확도 점수는 다음과 같습니다.")
-for i in range(0, len(SVM_kernel)) :
-    print("[" + SVM_kernel[i] + " 모델] ")
-    print("Acc : " + str(SVM_acc_list[i]) + " ({:.2f}%)".format(SVM_acc_list[i] * 100))
-    print("ROC AUC 점수(OVO(one-vs-one)) : " + str(SVM_roc_acc_ovo_list[i]) + " ({:.2f}%)".format(SVM_roc_acc_ovo_list[i] * 100))
-    print("ROC AUC 점수(OVR(one-vs-rest)) : " + str(SVM_roc_acc_ovr_list[i]) + " ({:.2f}%)".format(SVM_roc_acc_ovr_list[i] * 100))
-    print("")
+    print("\n")
 
-print("")
+    total = [SVM_acc, SVM_roc_acc_ovo, SVM_roc_acc_ovr]
 
-# Random Forest 모델 예측 및 점수
-print("Random Forest 모델에 대하여 예측합니다.")
-RF_pred = RF_model.predict(X_test_selected)
-RF_pred_prob = RF_model.predict_proba(X_test_selected)
+    return total
 
-RF_acc = accuracy_score(y_test, RF_pred)
-RF_roc_auc_ovo = roc_auc_score(y_test, RF_pred_prob, multi_class='ovo')
-RF_roc_auc_ovr = roc_auc_score(y_test, RF_pred_prob, multi_class='ovr')
 
-print("정확도 점수는 다음과 같습니다.")
-print("Acc : " + str(RF_acc) + " ({:.2f}%)".format(RF_acc * 100))
-print("ROC AUC 점수(OVO(one-vs-one)) : " + str(RF_roc_auc_ovo) + " ({:.2f}%)".format(RF_roc_auc_ovo * 100))
-print("ROC AUC 점수(OVR(one-vs-rest)) : " + str(RF_roc_auc_ovr) + " ({:.2f}%)".format(RF_roc_auc_ovr * 100))
+def RF_predict(RF_model, X_test_selected, y_test) :
+    # Random Forest 모델 예측 및 점수
+    print("Random Forest 모델에 대하여 예측합니다.")
+    RF_pred = RF_model.predict(X_test_selected)
+    RF_pred_prob = RF_model.predict_proba(X_test_selected)
 
-print("\n")
+    RF_acc = accuracy_score(y_test, RF_pred)
+    RF_roc_auc_ovo = roc_auc_score(y_test, RF_pred_prob, multi_class='ovo')
+    RF_roc_auc_ovr = roc_auc_score(y_test, RF_pred_prob, multi_class='ovr')
 
-# KNN(K-Nearest Neighbors) 모델 예측 및 점수
-print("KNN(K-Nearest Neighbors) 모델에 대하여 예측합니다.")
-KNN_pred = KNN_model.predict(X_test_selected)
-KNN_pred_prob = KNN_model.predict_proba(X_test_selected)
+    print("정확도 점수는 다음과 같습니다.")
+    print("Acc : " + str(RF_acc) + " ({:.2f}%)".format(RF_acc * 100))
+    print("ROC AUC 점수(OVO(one-vs-one)) : " + str(RF_roc_auc_ovo) + " ({:.2f}%)".format(RF_roc_auc_ovo * 100))
+    print("ROC AUC 점수(OVR(one-vs-rest)) : " + str(RF_roc_auc_ovr) + " ({:.2f}%)".format(RF_roc_auc_ovr * 100))
 
-KNN_acc = accuracy_score(y_test, KNN_pred)
-KNN_roc_auc_ovo = roc_auc_score(y_test, KNN_pred_prob, multi_class='ovo')
-KNN_roc_auc_ovr = roc_auc_score(y_test, KNN_pred_prob, multi_class='ovr')
+    print("\n")
 
-print("정확도 점수는 다음과 같습니다.")
-print("Acc : " + str(KNN_acc) + " ({:.2f}%)".format(KNN_acc * 100))
-print("ROC AUC 점수(OVO(one-vs-one)) : " + str(KNN_roc_auc_ovo) + " ({:.2f}%)".format(KNN_roc_auc_ovo * 100))
-print("ROC AUC 점수(OVR(one-vs-rest)) : " + str(KNN_roc_auc_ovr) + " ({:.2f}%)".format(KNN_roc_auc_ovr * 100))
+    total = [RF_acc, RF_roc_auc_ovo, RF_roc_auc_ovr]
+    return total
 
-print("\n")
+def KNN_predict(KNN_model, X_test_selected, y_test) :
+    # KNN(K-Nearest Neighbors) 모델 예측 및 점수
+    print("KNN(K-Nearest Neighbors) 모델에 대하여 예측합니다.")
+    KNN_pred = KNN_model.predict(X_test_selected)
+    KNN_pred_prob = KNN_model.predict_proba(X_test_selected)
 
-# XGBoost(eXtra Gradient Boost) 모델 예측 및 점수
-print("XGBoost(eXtra Gradient Boost) 모델에 대하여 예측합니다.")
-XGB_pred = XGB_model.predict(X_test_selected)
-XGB_pred_prob = XGB_model.predict_proba(X_test_selected)
+    KNN_acc = accuracy_score(y_test, KNN_pred)
+    KNN_roc_auc_ovo = roc_auc_score(y_test, KNN_pred_prob, multi_class='ovo')
+    KNN_roc_auc_ovr = roc_auc_score(y_test, KNN_pred_prob, multi_class='ovr')
 
-XGB_acc = accuracy_score(y_test, XGB_pred)
-XGB_roc_auc_ovo = roc_auc_score(y_test, XGB_pred_prob, multi_class='ovo')
-XGB_roc_auc_ovr = roc_auc_score(y_test, XGB_pred_prob, multi_class='ovr')
+    print("정확도 점수는 다음과 같습니다.")
+    print("Acc : " + str(KNN_acc) + " ({:.2f}%)".format(KNN_acc * 100))
+    print("ROC AUC 점수(OVO(one-vs-one)) : " + str(KNN_roc_auc_ovo) + " ({:.2f}%)".format(KNN_roc_auc_ovo * 100))
+    print("ROC AUC 점수(OVR(one-vs-rest)) : " + str(KNN_roc_auc_ovr) + " ({:.2f}%)".format(KNN_roc_auc_ovr * 100))
 
-print("정확도 점수는 다음과 같습니다.")
-print("Acc : " + str(XGB_acc) + " ({:.2f}%)".format(XGB_acc * 100))
-print("ROC AUC 점수(OVO(one-vs-one)) : " + str(XGB_roc_auc_ovo) + " ({:.2f}%)".format(XGB_roc_auc_ovo * 100))
-print("ROC AUC 점수(OVR(one-vs-rest)) : " + str(XGB_roc_auc_ovr) + " ({:.2f}%)".format(XGB_roc_auc_ovr * 100))
+    print("\n")
 
-print("\n")
+    total = [KNN_acc, KNN_roc_auc_ovo, KNN_roc_auc_ovr]
+    return total
+
+
+def XGB_predict(XGB_model, X_test_selected, y_test) :
+    # XGBoost(eXtra Gradient Boost) 모델 예측 및 점수
+    print("XGBoost(eXtra Gradient Boost) 모델에 대하여 예측합니다.")
+    XGB_pred = XGB_model.predict(X_test_selected)
+    XGB_pred_prob = XGB_model.predict_proba(X_test_selected)
+
+    XGB_acc = accuracy_score(y_test, XGB_pred)
+    XGB_roc_auc_ovo = roc_auc_score(y_test, XGB_pred_prob, multi_class='ovo')
+    XGB_roc_auc_ovr = roc_auc_score(y_test, XGB_pred_prob, multi_class='ovr')
+
+    print("정확도 점수는 다음과 같습니다.")
+    print("Acc : " + str(XGB_acc) + " ({:.2f}%)".format(XGB_acc * 100))
+    print("ROC AUC 점수(OVO(one-vs-one)) : " + str(XGB_roc_auc_ovo) + " ({:.2f}%)".format(XGB_roc_auc_ovo * 100))
+    print("ROC AUC 점수(OVR(one-vs-rest)) : " + str(XGB_roc_auc_ovr) + " ({:.2f}%)".format(XGB_roc_auc_ovr * 100))
+
+    print("\n")
+
+    total = [XGB_acc, XGB_roc_auc_ovo, XGB_roc_auc_ovr]
+
+    return total
--- src/train.py
+++ src/train.py
@@ -18,11 +18,13 @@ import mlflow
 import mlflow.sklearn
 
 # 추론 후 점수들
-from inference import LR_acc, LR_roc_auc_ovo, LR_roc_auc_ovr, \
-    SVM_acc_list, SVM_roc_acc_ovo_list, SVM_roc_acc_ovr_list, \
-    RF_acc, RF_roc_auc_ovo, RF_roc_auc_ovr, \
-    KNN_acc, KNN_roc_auc_ovo, KNN_roc_auc_ovr, \
-    XGB_acc, XGB_roc_auc_ovo, XGB_roc_auc_ovr
+# from inference import LR_predict, \
+#     SVM_acc_list, SVM_roc_acc_ovo_list, SVM_roc_acc_ovr_list, \
+#     RF_acc, RF_roc_auc_ovo, RF_roc_auc_ovr, \
+#     KNN_acc, KNN_roc_auc_ovo, KNN_roc_auc_ovr, \
+#     XGB_acc, XGB_roc_auc_ovo, XGB_roc_auc_ovr
+
+from inference import LR_predict, SVM_predict
 
 print("\n\n")
 print("[Train 단계 (train.py)]")
@@ -66,114 +68,103 @@ print("\n")
 # 모델 학습 단계
 # =====================================
 
-# 실험 준비 (tracking 준비)
-client = MlflowClient()
-experiment_id = client.create_experiment("Heart Disease Prediction(CardioCare datasets used)")
-
-experiment = client.get_experiment(experiment_id)
-
-print("모델 실험 ID : ", experiment_id)
-
 # 실험 이름 설정
-mlflow.set_experiment(experiment_id=experiment_id)
 
-# 실험 시작
+mlflow.set_experiment("Heart Disease Prediction(CardioCare datasets used) [Logistic Regression]")
+mlflow.autolog()
+
+# LR 실험 시작
 with mlflow.start_run() :
     # Logistic Regression 학습
     LR_model = LogisticRegression(max_iter=300, random_state=1)
     LR_model.fit(X_train_selected, y_train)
-    
+    print("Logistic Regression 모델 학습 완료")
+    print("\n")
+
     mlflow.log_params({"max_iter" : 300, "random_state" : 1})
+
+    mlflow.log_metric("Accuracy_Score", LR_predict(LR_model, X_test_selected, y_test)[0])
+    mlflow.log_metric("ROC_AUC_OVO", LR_predict(LR_model, X_test_selected, y_test)[1])
+    mlflow.log_metric("ROC_AUC_OVR", LR_predict(LR_model, X_test_selected, y_test)[2])
 
     mlflow.sklearn.log_model(LR_model, "model")
 
-    mlflow.log_metric("Accuracy Score", LR_acc)
-    mlflow.log_metric("ROC AUC(OVO)", LR_roc_auc_ovo)
-    mlflow.log_metric("ROC AUC(OVR)", LR_roc_auc_ovr)
+    print("Logistic Regression 모델 실험 완료\n")
 
-    print("Logistic Regression 모델 학습 완료")
 
-    print("\n")
+mlflow.set_experiment("Heart Disease Prediction(CardioCare datasets used) [SVM]")
+mlflow.autolog()
 
+with mlflow.start_run() :
     # SVM(Support Vector Machine) 학습
     SVM_kernel = ['linear', 'poly', 'rbf', 'sigmoid'] # precomputed 커널은 데이터가 정방행렬만 가능
     SVM_fitted_list = []
 
-    for i in range(0, len(SVM_kernel)) :
-        # SVC의 probality 속성이 scikit-learn 1.11 버전부터 deprecated 됨에 따라
-        # CalibratedClassifierCV() 함수로 이를 대체함
-
-        if (i == 'poly') :
-            SVM_model = CalibratedClassifierCV(SVC(kernel=SVM_kernel[i], C=5, degree=3, random_state=1), ensemble=False)
-        else :
-            SVM_model = CalibratedClassifierCV(SVC(kernel=SVM_kernel[i], C=5, random_state=1), ensemble=False)
-
-        SVM_model.fit(X_train_selected, y_train)
-
-        # 학습된 모델을 리스트화 해서 그대로 inference로 넘김
-        SVM_fitted_list.insert(i, SVM_model)
-
-        if (i == 'poly') :
-            mlflow.log_params({"kernel" : SVM_kernel[i],
-                               "C" : 5,
-                               "degree" : 3,
-                               "random_state" : 1})
-        else :
-            mlflow.log_params({"kernel" : SVM_kernel[i],
-                               "C" : 5,
-                               "random_state" : 1})
-        
-        mlflow.sklearn.log_model(SVM_model, "model")
-
-        mlflow.log_metric("Accuracy Score", SVM_acc_list[i])
-        mlflow.log_metric("ROC AUC(OVO)", SVM_roc_acc_ovo_list[i])
-        mlflow.log_metric("ROC AUC(OVR)", SVM_roc_acc_ovr_list[i])
+    SVM_model = CalibratedClassifierCV(SVC(kernel='rbf', C=5, random_state=1), ensemble=False)
+    SVM_model.fit(X_train_selected, y_train)
 
     print("SVM(Support Vector Machine) 모델 학습 완료")
-
     print("\n")
 
-    # Random Forest 학습 (앙상블 방법에서 사용된 모델 가져옴)
-    RF_model = forest
-    RF_model.fit(X_train_selected, y_train)
 
-    mlflow.log_params({"n_estimators" : 100, "max_depth" : 5, "random_state" : 1})
+    mlflow.log_params({"kernel" : 'rbf',
+                        "C" : 5,
+                        "random_state" : 1})
+        
+    mlflow.sklearn.log_model(SVM_model, "model", skops_trusted_types=[
+        "sklearn.calibration._CalibratedClassifier",
+        "sklearn.calibration._SigmoidCalibration",
+    ])
 
-    mlflow.sklearn.log_model(RF_model, "model")
+    mlflow.log_metric("Accuracy_Score", SVM_predict(SVM_model, X_test_selected, y_test)[0])
+    mlflow.log_metric("ROC_AUC_OVO", SVM_predict(SVM_model, X_test_selected, y_test)[1])
+    mlflow.log_metric("ROC_AUC_OVR", SVM_predict(SVM_model, X_test_selected, y_test)[2])
 
-    mlflow.log_metric("Accuracy Score", RF_acc)
-    mlflow.log_metric("ROC AUC(OVO)", RF_roc_auc_ovo)
-    mlflow.log_metric("ROC AUC(OVR)", RF_roc_auc_ovr)
+    print("SVM(Support Vector Machine) 모델 실험 완료\n")
 
-    print("Random Forest 모델 학습 완료")
-    print("\n")
 
-    # KNN(K-Nearest Neighbors) 학습
-    KNN_model = KNeighborsClassifier(n_neighbors=10)
-    KNN_model.fit(X_train_selected, y_train)
 
-    mlflow.log_param("n_neighbors", 10)
+    # # Random Forest 학습 (앙상블 방법에서 사용된 모델 가져옴)
+    # RF_model = forest
+    # RF_model.fit(X_train_selected, y_train)
 
-    mlflow.sklearn.log_model(KNN_model, "model")
+    # mlflow.log_params({"n_estimators" : 100, "max_depth" : 5, "random_state" : 1})
 
-    mlflow.log_metric("Accuracy Score", KNN_acc)
-    mlflow.log_metric("ROC AUC(OVO)", KNN_roc_auc_ovo)
-    mlflow.log_metric("ROC AUC(OVR)", KNN_roc_auc_ovr)
+    # mlflow.sklearn.log_model(RF_model, "model")
 
-    print("KNN(K-Nearest Neighbors) 모델 학습 완료")
-    print("\n")
+    # mlflow.log_metric("Accuracy Score", RF_acc)
+    # mlflow.log_metric("ROC AUC(OVO)", RF_roc_auc_ovo)
+    # mlflow.log_metric("ROC AUC(OVR)", RF_roc_auc_ovr)
 
-    # XGBoost(eXtra Gradient Boost) 학습 (설정은 Random Forest와 똑같이 하였음)
-    XGB_model = XGBClassifier(n_estimators=100, max_depth=5, random_state=1)
-    XGB_model.fit(X_train_selected, y_train)
+    # print("Random Forest 모델 학습 완료")
+    # print("\n")
 
-    mlflow.log_params({"n_estimators" : 100, "max_depth" : 5, "random_state" : 1})
+    # # KNN(K-Nearest Neighbors) 학습
+    # KNN_model = KNeighborsClassifier(n_neighbors=10)
+    # KNN_model.fit(X_train_selected, y_train)
+
+    # mlflow.log_param("n_neighbors", 10)
+
+    # mlflow.sklearn.log_model(KNN_model, "model")
+
+    # mlflow.log_metric("Accuracy Score", KNN_acc)
+    # mlflow.log_metric("ROC AUC(OVO)", KNN_roc_auc_ovo)
+    # mlflow.log_metric("ROC AUC(OVR)", KNN_roc_auc_ovr)
+
+    # print("KNN(K-Nearest Neighbors) 모델 학습 완료")
+    # print("\n")
+
+    # # XGBoost(eXtra Gradient Boost) 학습 (설정은 Random Forest와 똑같이 하였음)
+    # XGB_model = XGBClassifier(n_estimators=100, max_depth=5, random_state=1)
+    # XGB_model.fit(X_train_selected, y_train)
+
+    # mlflow.log_params({"n_estimators" : 100, "max_depth" : 5, "random_state" : 1})
     
-    mlflow.sklearn.log_model(XGB_model, "model")
+    # mlflow.sklearn.log_model(XGB_model, "model")
 
-    mlflow.log_metric("Accuracy Score", XGB_acc)
-    mlflow.log_metric("ROC AUC(OVO)", XGB_roc_auc_ovo)
-    mlflow.log_metric("ROC AUC(OVR)", XGB_roc_auc_ovr)
+    # mlflow.log_metric("Accuracy Score", XGB_acc)
+    # mlflow.log_metric("ROC AUC(OVO)", XGB_roc_auc_ovo)
+    # mlflow.log_metric("ROC AUC(OVR)", XGB_roc_auc_ovr)
 
-    print("XGBoost (eXtra Gradient Boost) 분류기 모델 학습 완료")
-    print("\n")
+    # print("XGBoost (eXtra Gradient Boost) 분류기 모델 학습 완료")
+    # print("\n")
